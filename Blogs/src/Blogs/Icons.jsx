0c6b5f9a659478b95dc39b328e367bad66904c77
SamadhanTikone <99667605+SamadhanTikone@users.noreply.github.com>
2024-11-21 18:39:05 +0530

    Delete Blogs directory

--- Blogs/src/Blogs/Icons.jsx
+++
@@ -1,9 +0,0 @@
-import { IoMdArrowRoundBack } from "react-icons/io";
-
-export default function Icons() {
-  return (
-    <div className="text-3xl p-0 m-0 hover:text-yellow-400">
-      <IoMdArrowRoundBack />
-    </div>
-  )
-}
